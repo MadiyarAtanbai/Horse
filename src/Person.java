3d2497300d63f73f4148c0c894c6510ef1438d4c
H0rsep0wer <madyk2002@mail.ru>
2024-02-27 22:43:46 +0600

    Adding

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Person {
+    public static void main(String[] args) {
+
+    }
+    String name;
+     int age;
+     String a="Madi";
+     int b=10;
+
+}
