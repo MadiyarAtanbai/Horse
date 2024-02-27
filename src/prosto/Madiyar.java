97412058223a8751af4561fa860eb3cd88a0ddf3
Madiyar <madyk2002@mail.ru>
2024-02-27 19:35:49 +0600

    добавил папку просто

---
+++ src/prosto/Madiyar.java
@@ -0,0 +1,19 @@
+package prosto;
+
+public class Madiyar {
+    private String name;
+    private int age ;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Madiyar(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
